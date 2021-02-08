6801a39b574a66a84155ff9f3e469254038eb928
davil <davil@172.16.19.243>
2021-02-08 09:17:21 +0100

    properties

---
+++ rest-ws-psp/src/main/java/com/pspro/webservice/RestWsPspApplication.java
@@ -0,0 +1,13 @@
+package com.pspro.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWsPspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWsPspApplication.class, args);
+	}
+
+}
